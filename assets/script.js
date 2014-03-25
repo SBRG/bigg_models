cff0448d53e7c361382eadeb0800681b259db576
Justin Lu <justinlu10@gmail.com>
2014-03-25 12:30:02 -0700

    adding a typeahead functionality prototype

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 		$('.navbar').css({position: setPos});
 	});
 	
-	$('.carousel').carousel({interval: 8000});
+	$('.carousel').carousel({interval: 9000});
 		
 	$( "#showmodels" ).click(function () {
 			
@@ -19,6 +19,29 @@ $( document ).ready(function() {
 		}
 	});
 	
+	var models = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+   prefetch: {
+    // url points to a json file that contains an array of country names, see
+    // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+    url: 'http://localhost:8888/api/models',
+    // the json file contains an array of strings, but the Bloodhound
+    // suggestion engine expects JavaScript objects so this converts all of
+    // those strings
+    filter: function(list) {
+      return $.map(list, function(model) { return { name: model }; });
+   }
+   }
+   });
+   models.initialize();
+	$('#multiple-datasets .typeahead').typeahead(null, {
+  name: 'models',
+  displayKey: 'name',
+  // `ttAdapter` wraps the suggestion engine in an adapter that
+  // is compatible with the typeahead jQuery plugin
+  source: models.ttAdapter()
+});
 });
 
 
