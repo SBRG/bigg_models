345fb601ff8f903622d5c8157d9d07b3e6573964
Zachary King <zaking17@gmail.com>
2019-09-13 17:58:20 -0700

    fix for server

--- bigg_models/queries.py
+++ bigg_models/queries.py
@@ -189,7 +189,7 @@ def get_model_reactions(
         size=None,
         sort_column=None,
         sort_direction='ascending',
-        **kwargs,
+        **kwargs
 ):
     """Get model reactions.
 
